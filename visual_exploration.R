f59a1aeff8d75f362d7b691e07d28dde2a11c85b
Louis Philipp Lukas <louis.lukas@bsse.ethz.ch>
2021-11-04 17:02:08 +0100

    start adapting script for use w/ EMSCI
    Add more flexible data loading to easily switch between Sygen and EMSCI.
    
    Use more generic variable names (e.g. data instead of sygen_raw). Try to
    introduce consistency in variable naming (e.g. variable.dist for simple
    descriptive plots).
    
    Restyle estimated density plots comparing LEMS week 1 and week 52 by AIS
    grade: use scaled density for better comparability; add group sizes to
    plots.

--- visual_exploration.R
+++ visual_exploration.R
@@ -1,70 +1,134 @@
 ################################################################################
-# SCI - Handling missing data project 
-# L. Bourguignon & L.P. Lukas 
+# SCI - Handling missing data project
+# L. Bourguignon & L.P. Lukas
 # First version : 31.10.2021
 # Last update : 31.10.2021
 # ------------------------------------------------------------------------------
 # VISUALE EXPLORATION
 ################################################################################
 
+library(tidyverse)
+library(cowplot)
+library(finalfit)
+library(naniar)
+# custom functions
+source("functions.R")
+
+data.set <- "emsci" # one of: sygen, emsci
+path.to.data = "/Volumes/borgwardt/Data/SCI/"
+
 ################################################################################
 # Data loading
 ################################################################################
 
-data_path = '/Volumes/borgwardt/Data/SCI/'
-sygen_raw <- read.csv(paste(data_path, 'Sygen/JohnKramersProject_DATA_2019-10-07_0111.csv', sep = ''))
+if (data.set == "sygen") {
+    injury_levels  <-  c(paste(rep("C0", 8), 1:8, sep = ""),
+                         paste(rep("T0", 9), 1:9, sep = ""),
+                         paste(rep("T", 3), 10:12, sep = ""))
+    AIS_grades <- c("AIS A", "AIS B", "AIS C", "AIS D")
+    data <- read_csv(paste(path.to.data,
+                           "Sygen/JohnKramersProject_DATA_2019-10-07_0111.csv",
+                           sep = ""),
+                     col_types = list(ptid =  col_character(),
+                                      age = col_integer(),
+                                      sexcd = col_factor(levels = c("1", # male
+                                                                    "2") # female
+                                      ),
+                                      splvl = col_factor(levels = injury_levels),
+                                      ais1 = col_factor(levels = AIS_grades),
+                                      lower01 = col_integer(),
+                                      lower52 = col_integer()),
+                     col_select = c("ptid",
+                                    "age",
+                                    "sexcd",
+                                    "splvl",
+                                    "ais1",
+                                    "lower01",
+                                    "lower52"),
+                     na = c("", "NA", "ND")) %>%
+        mutate(
+            across(starts_with("ais"),
+                   ~recode(.,
+                           "AIS A" = "A",
+                           "AIS B" = "B",
+                           "AIS C" = "C",
+                           "AIS D" = "D")
+            )
+        )
+} else if (data.set == "emsci") {
+    injury_levels <- c(paste(rep("C", 8), 1:8, sep = ""),
+                       paste(rep("T", 12), 1:12, sep = ""),
+                       paste(rep("L", 5), 1:5, sep = ""),
+                       paste(rep("S", 5), 1:5, sep = ""))
+    AIS_grades <- c("A", "B", "C", "D", "E")
+    data <- read_csv(paste(path.to.data,
+                           "EMSCI/emsci_data_sygen_format.csv",
+                           # "Sygen/JohnKramersProject_DATA_2019-10-07_0111.csv",
+                           sep = ""),
+                     col_types = list(ptid =  col_integer(),
+                                      age = col_integer(),
+                                      sexcd = col_factor(levels = c("1", # male
+                                                                    "2") # female
+                                      ),
+                                      splvl = col_factor(levels = injury_levels),
+                                      ais1 = col_factor(levels = AIS_grades),
+                                      lower01 = col_integer(),
+                                      lower52 = col_integer()),
+                     na = c("", "NA", "INT") # INT - intermediate; cannot be
+                     # resolved to NLI with information contained in EMSCI?
+    )
+} else {
+    data <- NA
+    print("Invalid data set identifier! Must be 'sygen' or 'emsci'.")
+}
+# add the aggregated level attribute
+data <- data %>%
+    mutate(level = case_when(
+        grepl('C', splvl, fixed = TRUE) ~ "cervical",
+        grepl('T', splvl, fixed = TRUE) ~ "thoracic",
+        grepl('L', splvl, fixed = TRUE) ~ "lumbar")
+    ) %>%
+    mutate(level = factor(level,
+                          levels = c("cervical", "thoracic", "lumbar"))
+    )
 
 ################################################################################
-# Sourcing additional scripts
-################################################################################
-
-source("/Users/blucie/PhD/1_SCI/6_Missing_data/code/functions.R")
-
-################################################################################
-# Visualise raw Sygen data
+# Visualise raw data
 ################################################################################
 
 ## Visualise original missingness patterns
-
-# ref for interpretation of the folloming plot: 
+# reference for interpretation of the following plot:
 # https://cran.r-project.org/web/packages/finalfit/vignettes/missing.html
-
-missing_dist <- sygen_analysis %>% 
+missing_dist <- data %>%
   missing_pairs("lower52", c("age", "sexcd", "level", "lower01", "ais1"))
-
-missing_dist_fill <- sygen_analysis %>% 
-  missing_pairs("lower52", c("age", "sexcd", "level", "lower01", "ais1"), 
+missing_dist_fill <- data %>%
+  missing_pairs("lower52", c("age", "sexcd", "level", "lower01", "ais1"),
                 position = "fill", )
-
 # x-axis represent patients, light blue = NA
-missing_heatmap <-sygen_analysis %>%
+missing_heatmap <- data %>%
   missing_plot()
 
 # ------------------------------------------------------------------------------
 
-## Visualise the distributions of the different variables
-
-# AIS grade, i.e. severity
-ais_sygen_raw <- ggplot(sygen_analysis, aes(ais1)) + geom_bar()
-
-# Age distribution
-age_sygen_raw <- ggplot(sygen_analysis, aes(x=age)) + 
+## Visualise the distributions of the different variables at baseline
+# AIS grade
+ais1.dist <- ggplot(data, aes(ais1)) + geom_bar()
+# Age
+age.dist <- ggplot(data, aes(x=age)) +
   geom_histogram(aes(y=..density..), colour="black", fill="white")+
-  geom_density(alpha=.2, fill="#FF6666") 
-
-# Sex 
-sex_sygen_raw <- ggplot(sygen_analysis, aes(factor(sexcd))) + geom_bar() + 
+  geom_density(alpha=.2, fill="#FF6666")
+# Sex
+sex.dist <- ggplot(data, aes(factor(sexcd))) + geom_bar() +
   scale_x_discrete(labels=c("1" = "Female", "2" = "Male"), name = 'Sex')
-
-# Distribution of LEMS at week 01
-lems01_sygen_raw <- ggplot(sygen_analysis, aes(x=lower01)) + geom_density(alpha=.2, fill="#FF6666") + xlim(0,50)
-
-# Distribution of LEMS at week 52
-lems52_sygen_raw <- ggplot(sygen_analysis, aes(x=lower52)) + geom_density(alpha=.2, fill="#FF6666") + xlim(0,50)
-
+# LEMS at week 01
+lems01.dist <- ggplot(data, aes(x=lower01)) +
+    geom_density(alpha=.2, fill="#FF6666") + xlim(0,50)
+# LEMS at week 52
+lems52.dist <- ggplot(data, aes(x=lower52)) +
+    geom_density(alpha=.2, fill="#FF6666") + xlim(0,50)
 # Level of injury
-nli_sygen_raw <- ggplot(sygen_analysis, aes(splvl)) + geom_bar() + 
-  scale_x_discrete(name = 'Level of injury')
+nli.dist <- ggplot(data, aes(splvl)) + geom_bar() +
+  scale_x_discrete(name = 'NLI')
 
 # Variables stratified by AIS grade
 #ggplot(sygen_analysis, aes(x=lower01, fill=ais1)) + geom_density(alpha=.3)+ xlim(0,50)
@@ -74,103 +138,124 @@ nli_sygen_raw <- ggplot(sygen_analysis, aes(splvl)) + geom_bar() +
 #ggplot(sygen_analysis, aes(x=splvl, fill=ais1)) + geom_histogram(stat="count")
 
 # Relationship between LEMS at week 01 and week 52, coloured by AIS grade
-jitterLEMS_byAIS_sygen_raw <- ggplot(sygen_analysis, aes(lower01, lower52, colour=ais1)) + 
-  geom_jitter(width = 2, height = 2) + 
+LEMS.by.AIS <- ggplot(data, aes(lower01, lower52, colour=ais1)) +
+  geom_jitter(width = 2, height = 2) +
   xlim(-2,50) +
   ylim(-2,50)
 
 # Change in LEMS distribution over time, by AIS grade
-lems01_byAIS_sygen_raw <- ggplot(sygen_analysis, aes(x=lower01)) + geom_density(alpha=.2, fill="#FF6666") + 
-  xlim(0,50) +
-  facet_grid(rows = vars(ais1), scales = 'free')
-lems52_byAIS_sygen_raw <- ggplot(sygen_analysis, aes(x=lower52)) + geom_density(alpha=.2, fill="#FF6666") + 
-  xlim(0,50) +
-  facet_grid(rows = vars(ais1), scales = 'free')
-distLEMS_byAIS_sygen_raw <- ggarrange(lems01_byAIS_sygen_raw, lems52_byAIS_sygen_raw, ncol = 2, nrow = 1)
-
+lems01.by.AIS <- ggplot(data) +
+    geom_density(aes(x=lower01, y=..scaled..), # use ..scaled.. for better
+                 alpha=.2, fill="#FF6666") + # comparability between AIS grades
+    xlim(-1, 50) +
+    ylim(0, 1.1) +
+    xlab("LEMS (week 1)") +
+    ylab("Density (scaled)") +
+    facet_grid(rows = vars(ais1), scales = 'fixed')
+ais.counts <- data %>%
+    group_by(ais1) %>%
+    count() %>%
+    mutate(n = paste("n = ", n, sep = ""))
+lems52.by.AIS <- ggplot(data) +
+    geom_density(aes(x=lower52, y=..scaled..),
+                 alpha=.2, fill="#FF6666") +
+    xlim(-1, 50) +
+    ylim(0, 1.1) +
+    xlab("LEMS (week 52)") +
+    facet_grid(rows = vars(ais1), scales = 'fixed') +
+    geom_text(data = ais.counts,
+              mapping = aes(x = 25, y = .975, label = n),
+              ) +
+    theme(axis.title.y = element_blank()) # remove y-axis label
+lems.by.AIS.dist <- plot_grid(lems01.by.AIS,
+                              lems52.by.AIS,
+                              ncol = 2, nrow = 1)
+lems.by.AIS.dist
 # ------------------------------------------------------------------------------
-
-## Visualise the co-occurrence of missingness
-
-sygen_copy <- sygen_raw
-sygen_copy[sygen_copy == ''] <- NA
-sygen_copy[sygen_copy == 'ND'] <- NA
+## Visualise the co-occurrence of missingness (ONLY Sygen)
+# previous visualisations worked w/ smaller subset of variables; EMSCI only
+# adapted on this subset (as of 04 November)
+sygen <- read.csv(paste(path.to.data,
+                        'Sygen/JohnKramersProject_DATA_2019-10-07_0111.csv',
+                        sep = ''))
+sygen[sygen == ''] <- NA
+sygen[sygen == 'ND'] <- NA
 
 # Define vectors with column names of the different category of variables
 vec_lems <- c('lower01', 'lower04', 'lower08', 'lower16', 'lower26', 'lower52') # all LEMS
 vec_ais <- c('ais1', 'ais4', 'ais8', 'ais16', 'ais26', 'ais52') # all AIS grades
 vec_mot <- c('ankdol', 'ankdor', 'ankpll', 'ankplr',
-             'elbexl', 'elbexr', 'elbfll', 'elbflr', 
-             'finabl', 'finabr','finfll', 'finflr', 
-             'gretol', 'gretor','hipfll', 'hipflr', 
+             'elbexl', 'elbexr', 'elbfll', 'elbflr',
+             'finabl', 'finabr','finfll', 'finflr',
+             'gretol', 'gretor','hipfll', 'hipflr',
              'kneetr', 'kneexl', 'wrextl', 'wrextr') # all myotomes
 sygen_levels <- c('c2', 'c3', 'c4', 'c4', 'c6', 'c7', 'c8',
                   't1', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9', 't10', 't11', 't12',
                   'l1', 'l2', 'l3', 'l4', 'l5',
                   's1', 's2', 's3', 's45') # all levels evaluated in sensory scores
-vec_pp <- append(vector_var(sygen_levels, 'ppl', 'after'), 
+vec_pp <- append(vector_var(sygen_levels, 'ppl', 'after'),
                  vector_var(sygen_levels, 'ppr', 'after')) # all dermatomes for pin prick
-vec_lt <- append(vector_var(sygen_levels, 'ltl', 'after'), 
+vec_lt <- append(vector_var(sygen_levels, 'ltl', 'after'),
                  vector_var(sygen_levels, 'ltr', 'after')) # all dermatomes for light touch
 
 # ------------------------------------------------------------------------------
 # WILL BE INCLUDED IN FUNCTION FILE
 plot_miss_upset <- function(data, cols){
-  
+
   df_temp <- subset_col(cols, data)
-  
+
   plot <- gg_miss_upset(df_temp, nsets = n_var_miss(df_temp))
-  
+
   return (plot)
 }
 # ------------------------------------------------------------------------------
 
 # co-occurring NAs among AIS measurements over time
-miss_upset_ais_overtime <- plot_miss_upset(sygen_copy, vec_ais)
-miss_upset_ais_overtime_A <- plot_miss_upset(sygen_copy[sygen_copy$ais1 == 'AIS A', ], 
+miss_upset_ais_overtime <- plot_miss_upset(sygen, vec_ais)
+miss_upset_ais_overtime_A <- plot_miss_upset(sygen[sygen$ais1 == 'AIS A', ],
                                              vec_ais)
 
 # co-occurring NAs among LEMS measurements over time
-miss_upset_lems_overtime <- plot_miss_upset(sygen_copy, vec_lems)
+miss_upset_lems_overtime <- plot_miss_upset(sygen, vec_lems)
 # co-occurring NAs among LEMS and AIS measurements over time
-miss_upset_lems_ais_overtime <- plot_miss_upset(sygen_copy, 
-                                                c(vec_lems, vec_ais, 
+miss_upset_lems_ais_overtime <- plot_miss_upset(sygen,
+                                                c(vec_lems, vec_ais,
                                                   'vaccd01', 'anyana01'))
 # co-occurring NAs among myotomes at week 00
-miss_upset_myotomes_00 <- plot_miss_upset(sygen_copy, paste0(vec_mot, '00'))
+miss_upset_myotomes_00 <- plot_miss_upset(sygen, paste0(vec_mot, '00'))
 # co-occurring NAs among myotomes at week 52
-miss_upset_myotomes_52 <- plot_miss_upset(sygen_copy, paste0(vec_mot, '52'))
+miss_upset_myotomes_52 <- plot_miss_upset(sygen, paste0(vec_mot, '52'))
 # co-occurring NAs among pin prink dermatomes at week 01
-miss_upset_pinprick_01 <- plot_miss_upset(sygen_copy, paste0(vec_pp, '01')) # always all missing together
+miss_upset_pinprick_01 <- plot_miss_upset(sygen, paste0(vec_pp, '01')) # always all missing together
 # co-occurring NAs among pin prink dermatomes at week 52
-miss_upset_pinprick_52 <- plot_miss_upset(sygen_copy, paste0(vec_pp, '52'))
+miss_upset_pinprick_52 <- plot_miss_upset(sygen, paste0(vec_pp, '52'))
 # co-occurring NAs among light touch dermatomes at week 00
-miss_upset_lighttouch_01 <- plot_miss_upset(sygen_copy, paste0(vec_lt, '01'))
+miss_upset_lighttouch_01 <- plot_miss_upset(sygen, paste0(vec_lt, '01'))
 # co-occurring NAs among light touch dermatomes at week 52
-miss_upset_lighttouch_52 <- plot_miss_upset(sygen_copy, paste0(vec_lt, '52'))
+miss_upset_lighttouch_52 <- plot_miss_upset(sygen, paste0(vec_lt, '52'))
 
 # LEMS dataframe and overview heatmap
-sygen_lems_overtime <- subset_col(vec_lems, sygen_raw)
+sygen_lems_overtime <- subset_col(vec_lems, sygen)
 sygen_lems_overtime %>%
   missing_plot()
 vis_miss(sygen_lems_overtime)
 
 # AIS grade dataframe and overview heatmap
-sygen_ais_overtime <- subset_col(vec_ais, sygen_raw)
+sygen_ais_overtime <- subset_col(vec_ais, sygen)
 sygen_ais_overtime %>%
   missing_plot()
 vis_miss(sygen_ais_overtime)
 
 # LEMS and AIS grade dataframe + VAC and DAP
-sygen_lems_ais_overtime <- subset_col(c(vec_lems, vec_ais, 
-                                        'vaccd01', 'anyana01'), sygen_raw)
+sygen_lems_ais_overtime <- subset_col(c(vec_lems, vec_ais,
+                                        'vaccd01', 'anyana01'), sygen)
 
 # -------------------------
 
 # Sankey plot : transition in AIS grades from week 01 to week 26
 
 # Remove "AIS" from the values of the AIS grades columns
-sygen_ais_overtime <- sygen_ais_overtime %>% 
+sygen_ais_overtime <- sygen_ais_overtime %>%
   mutate_all(funs(str_replace(., "AIS ", "")))
 
 # renaming the variable values such that they include information on the week when it was collected
@@ -182,29 +267,29 @@ sygen_ais_overtime$ais26 <- paste(sygen_ais_overtime$ais26, "_26", sep="")
 sygen_ais_overtime$ais52 <- paste(sygen_ais_overtime$ais52, "_52", sep="")
 
 # Create a table with :
-# source (i.e. AIS grade at week 1), 
+# source (i.e. AIS grade at week 1),
 # target (i.e. AIS grade at week 26)
 # value (i.e. number of patients go from one category to another from week 1 to 26)
-transition1_26 <- sygen_ais_overtime %>% 
-  group_by(ais1) %>% 
+transition1_26 <- sygen_ais_overtime %>%
+  group_by(ais1) %>%
   count(ais26)
 transition1_26 <- as.data.frame(transition1_26)
 names(transition1_26) <- c('source', 'target', 'value')
 
 # From these flows we need to create a node data frame: it lists every entities involved in the flow
 nodes <- data.frame(
-  name=c(as.character(transition1_26$source), 
+  name=c(as.character(transition1_26$source),
          as.character(transition1_26$target)) %>% unique()
 )
 
 # With networkD3, connection must be provided using id, not using real name like in the links dataframe.. So we need to reformat it.
-transition1_26$IDsource <- match(transition1_26$source, nodes$name)-1 
+transition1_26$IDsource <- match(transition1_26$source, nodes$name)-1
 transition1_26$IDtarget <- match(transition1_26$target, nodes$name)-1
 
 # Make the Network
 sankey_transition_1_26 <- sankeyNetwork(Links = transition1_26, Nodes = nodes,
                    Source = "IDsource", Target = "IDtarget",
-                   Value = "value", NodeID = "name", 
+                   Value = "value", NodeID = "name",
                    sinksRight=T, iterations = 0)
 sankey_transition_1_26
 
@@ -219,9 +304,9 @@ sygen_lems_overtime_binary[(is.na(sygen_lems_overtime_binary))] <- 0
 # Here are also plotted points:
 # below 0 on x-axis : lems at week 01 known, NA for lems at week 52
 # below 0 on y-axis : NA for lems at week 01, lems at week 52 known
-ggplot(sygen_ais_lems_overtime, aes(lower01, lower52)) + 
-  #geom_jitter(width = 2, height = 2) + 
-  geom_miss_point() + 
+ggplot(sygen_ais_lems_overtime, aes(lower01, lower52)) +
+  #geom_jitter(width = 2, height = 2) +
+  geom_miss_point() +
   facet_wrap(~ais1)
 
 # Add information about severity grade at week 01 to the binarised lems dataframe
@@ -230,8 +315,8 @@ sygen_lems_overtime_binary_ais1$ais1 <- sygen_raw$ais1
 
 # Transform lems info overtime to patterns representing sequence of LEMS measurements
 # e.g. 0-1-1-1-1-1 means LEMS is missing at week 01 but present for all following time points
-lems_na_pattern_ais1 <- sygen_lems_overtime_binary_ais1 %>% 
-  group_by(lower01, lower04, lower08, lower16, lower26, lower52, ais1) %>% 
+lems_na_pattern_ais1 <- sygen_lems_overtime_binary_ais1 %>%
+  group_by(lower01, lower04, lower08, lower16, lower26, lower52, ais1) %>%
   count()
 lems_na_pattern_ais1$pattern <- apply( lems_na_pattern_ais1[ , vec_lems ] , 1 , paste , collapse = "-" )
 # Remove pattern with all measurements available since we are interested in patterns with NAs
@@ -245,9 +330,9 @@ lems_na_pattern_ais1_withna <- lems_na_pattern_ais1[!(lems_na_pattern_ais1$patte
 lems_na_pattern_ais1_withna_aisnonena <- lems_na_pattern_ais1_withna[!(is.na(lems_na_pattern_ais1_withna$ais1)),]
 
 # Plot heatmap: number of patients per missing pattern per AIS grade
-ggplot(lems_na_pattern_ais1_withna, aes(x=ais1, y=pattern, fill=n)) + 
+ggplot(lems_na_pattern_ais1_withna, aes(x=ais1, y=pattern, fill=n)) +
   geom_tile() +
-  geom_text(aes(label = n, color='white')) 
+  geom_text(aes(label = n, color='white'))
 
 # Decision tree that best determine the proportion of missingness in patients
 sygen_lems_ais_overtime[sygen_lems_ais_overtime == ""] <- NA
@@ -256,5 +341,3 @@ sygen_lems_ais_overtime %>%
   add_prop_miss() %>%
   rpart(prop_miss_all ~ ., data = .) %>%
   prp(type = 4, extra = 101, prefix = "Prop. Miss = ")
-
-
