73874ba20e7aacc2aa9d49d106920ad4bd657907
lbourguignon <43504691+lbourguignon@users.noreply.github.com>
2021-11-24 10:29:08 +0100

    working plot fit + added last_obs carried forward
    - solved the problem in plotting function: results can now be plotted and output
    - added last observation carried forward as an imputation method for missingness in LEMS at week 52 (carries LEMS at week 26 forward)
    - added upper bound of 1 for probability of NAs in functions introducing NAs

--- functions.R
+++ functions.R
@@ -1,8 +1,8 @@
 ################################################################################
 # SCI - Handling missing data project 
-# L. Bourguignon
+# L. Bourguignon & L.P. Lukas 
 # First version : 06.07.2021
-# Last update : 12.10.2021
+# Last update : 31.10.2021
 # ------------------------------------------------------------------------------
 # FUNCTIONS
 ################################################################################
@@ -91,6 +91,11 @@ ais_NAR <- function(x, weight_a, weight_b, weight_c, weight_d){
 
 # ------------------------------------------------------------------------------
 
+# Function to introduce NAs not at random (high score more likely to be missing) 
+# from column col in dataframe `df`
+# This function is called when the quantile for the proportion of NA required
+# is equal to 0
+# Output is a vector 
 lems_NAR_0quantile <- function(x, proba_lowlems, proba_highlems){
   if (x == 0){
     x <- sample(c(0, NA), 
@@ -107,6 +112,11 @@ lems_NAR_0quantile <- function(x, proba_lowlems, proba_highlems){
 
 # ------------------------------------------------------------------------------
 
+# Function to introduce NAs not at random (high score more likely to be missing) 
+# from column col in dataframe `df`
+# This function is called when the quantile for the proportion of NA required
+# is NOT equal to 0
+# Output is a vector 
 lems_NAR_none0quantile <- function(x, proba_lowlems, proba_highlems){
   if (x < quantile){
     x <- sample(c(0, NA), 
@@ -123,6 +133,13 @@ lems_NAR_none0quantile <- function(x, proba_lowlems, proba_highlems){
 
 # ------------------------------------------------------------------------------
 
+# Function to introduce NAs not at random (high score more likely to be missing) 
+# from column col in dataframe `df`
+# This function calls separate functions for categorial (AIS grade) and numerical
+# (lower01, lower52) variables
+# col : column in which to introduce missingness (string)
+# prop : proportion of missingness to be introduced (integer between 0 and 100)
+# Output is a vector
 generate_missing_col_NAR <- function(data, prop, col){
   if (col == 'ais1'){
     
@@ -135,6 +152,12 @@ generate_missing_col_NAR <- function(data, prop, col){
     weight_b = 2*weight_a
     weight_c = 3*weight_a
     weight_d = 4*weight_a
+    # If the generated weight for AIS D is more than 1, 
+    # then the probability of not missing will be negative
+    # Thus, we readjust suchh that the probability of not missing is 0 instead
+    if (weight_d > 1){
+      weight_d = 1
+    }
     data$test <- apply(data['test'], 1, ais_NAR, weight_a, weight_b, weight_c, weight_d)
     
   } else if (col %in% c('lower01', 'lower52')){
@@ -148,13 +171,20 @@ generate_missing_col_NAR <- function(data, prop, col){
       indx <- indx/100
       proba_lowlems <- (prop)/(4*(1-indx)+indx)
       proba_highlems <- proba_lowlems*4 # the probability of having NA when lems is high is four times higher than when lems is low
+      # Probability of missing should be bounded by 1
+      if (proba_highlems > 1){
+        proba_highlems = 1
+      }
       
       data$test <- apply(data['test'], 1, lems_NAR_0quantile, proba_lowlems, proba_highlems)
       
     } else {
       proba_lowlems <- (prop)/(4*(1-prop)+prop)
       proba_highlems <- proba_lowlems*4 # the probability of having NA when lems is high is four times higher than when lems is low
-      
+      # Probability of missing should be bounded by 1
+      if (proba_highlems > 1){
+        proba_highlems = 1
+      }
       data$test <- apply(data['test'], 1, lems_NAR_none0quantile, proba_lowlems, proba_highlems)
     }
   }
@@ -166,6 +196,12 @@ generate_missing_col_NAR <- function(data, prop, col){
 
 # ------------------------------------------------------------------------------
 
+# Function to introduce NAs 
+# This function calls separate functions for different patterns of missingness
+# cols : columns in which to introduce missingness (vector)
+# patterns : patterns of missingness (vector)
+# prop : proportion of missingness to be introduced (integer between 0 and 100)
+# Output is a data frame
 introduce_missingness <- function(data, cols, patterns, prop){
   if ('MCAR' %in% patterns){
     for (col in cols){
@@ -188,18 +224,29 @@ introduce_missingness <- function(data, cols, patterns, prop){
 
 # ------------------------------------------------------------------------------
 
+# Function to format the data before fitting linear model
+# var : variable in which NAs were introduced (string)
+# pat : pattern used to introduce NAs (string)
+# variables : all potential variables in which NAs can be found (vector)
+# Output is a data frame
 prepare_df_lm <- function(data_sub, var, pat, variables){
-  data_temp <- subset_col(c('age', 'sexcd', 'level',
-                            variables[-(match(var, variables))], 
-                            paste0(var, '_', pat)),
+  data_temp <- subset_col(c('age', 'sexcd', 'level', # baseline characteristics not affected by NAs and always in the models
+                            variables[-(match(var, variables))], # other variables not affected by NAs
+                            paste0(var, '_', pat)), # variable var with NAs introduced with pat pattern
                           data_sub)
   return (data_temp)
 }
 
 # ------------------------------------------------------------------------------
 
+# Function for fitting linear model
+# var : variable in which NAs were introduced (string)
+# pat : pattern used to introduce NAs (string)
+# variables : all potential variables in which NAs can be found (vector)
+# imput : full name of the imputation method (string)
+# Output is a data frame (summary of the fitted model)
 run_lm <- function(data_temp, var, pat, variables, imput){
-  if (var != 'lower52'){
+  if (var != 'lower52'){ # formulas are slightly different depending on if the outcome variable has NAs or not
     formula <- paste0(grep('lower52', variables, value=TRUE)," ~ .") 
   } else if (var == 'lower52'){
     formula <- paste0(paste0(grep(var, variables, value=TRUE), '_', pat)," ~ .") 
@@ -211,16 +258,25 @@ run_lm <- function(data_temp, var, pat, variables, imput){
 
 # ------------------------------------------------------------------------------
 
-result_imputations <- function(data, var, patterns, imp){
+# Function for extracting results of linear models using imputed NAs 
+# var : variable in which NAs were introduced (string)
+# patterns : patterns used for introducing missingness (vector)
+# imp : imputation methods used (vector)
+# Output is two data frames (combined summaries of the fitted models & variables table)
+result_imputations <- function(data, var, patterns, imp, raw = NULL){
   data_copy <- data
+  # Baseline model (ground truth, no missing values)
   baseline <- summary(lm(lower52 ~ lower01 + level + ais1 + age + sexcd, data = data))
   
+  # Prepare data frame to store the results from the fitted models
   df_results <- data.frame(matrix(, nrow=8, ncol=0))
   df_results$variables <- rownames(baseline$coef)
   df_results$coef <- baseline$coefficients[,1]
   df_results$pvalues <- baseline$coefficients[,4]
   df_results$model <- c(rep('baseline', 8))
   
+  # (Current) potential variables in which NAs where introduced 
+  ### FIND A WAY TO NOT HARD CODE THAT PART ###
   variables <- c('lower01', 'lower52', 'ais1')
   
   data_sub <- subset_col(c('ptid', 'age', 'sexcd', 'level', 'ais1', 'lower01', 'lower52',
@@ -231,7 +287,6 @@ result_imputations <- function(data, var, patterns, imp){
     for (pat in patterns){
       data_temp <- prepare_df_lm(data_sub, var, pat, variables)
       extracted_summary <- run_lm(data_temp, var, pat, variables, 'case_deletion')
-      ### Find a solution for the non matching variables names for the plots
       df_results <- rbind(df_results, extracted_summary)
     }
   }
@@ -249,7 +304,6 @@ result_imputations <- function(data, var, patterns, imp){
       data_copy[paste0(var, '_', pat, '_mean/majority')] <- data_temp[paste0(var, '_', pat)] %>% replace(is.na(.), replaceby)
       data_temp[paste0(var, '_', pat)] <- data_temp[paste0(var, '_', pat)] %>% replace(is.na(.), replaceby)
       extracted_summary <- run_lm(data_temp, var, pat, variables, 'mean/majority')
-      ### Find a solution for the non matching variables names for the plots
       df_results <- rbind(df_results, extracted_summary)
     }
   }
@@ -272,7 +326,6 @@ result_imputations <- function(data, var, patterns, imp){
         formula <- paste0(paste0(grep(var, variables, value=TRUE), '_', pat)," ~ .") 
         model <- lm(formula, data = data_temp_nooutcome_complete)
       } else if (sapply(data_temp, typeof)[paste0(var, '_', pat)][[1]] == 'character'){
-        #data_temp_nooutcome_complete[paste0(var, '_', pat)] <- factor(data_temp_nooutcome_complete[paste0(var, '_', pat)])
         formula <- paste0(paste0(grep(var, variables, value=TRUE), '_', pat)," ~ .") 
         model <- multinom(formula, data = data_temp_nooutcome_complete, family = 'poisson')
       }
@@ -281,7 +334,6 @@ result_imputations <- function(data, var, patterns, imp){
       data_temp_nooutcome_missing_imputed[paste0(var, '_', pat)] <- predict(model, newdata = data_temp_nooutcome_missing)
       data_temp_nooutcome_imputed <- rbind(data_temp_nooutcome_complete, data_temp_nooutcome_missing_imputed)
       
-      ## not sure this part is working ##
       data_temp_nooutcome_imputed$index <- as.numeric(row.names(data_temp_nooutcome_imputed))
       data_temp_nooutcome_imputed <- data_temp_nooutcome_imputed[order(data_temp_nooutcome_imputed$index), ]
       if (var != 'lower52'){
@@ -296,6 +348,24 @@ result_imputations <- function(data, var, patterns, imp){
     }
   }
   
+  if ('last_obs_forward' %in% imp){
+    for (pat in patterns){
+      
+      temp <- subset(raw, select=c(ptid,lower26))
+      data_sub <- merge(data_sub, temp)
+      data_temp <- prepare_df_lm(data_sub, var, pat, variables)
+      data_temp$lower26 <- data_sub$lower26
+      
+      data_temp[paste0(var, '_', pat)][is.na(data_temp[paste0(var, '_', pat)])] <- data_temp['lower26'][is.na(data_temp[paste0(var, '_', pat)])]
+      data_copy[paste0(var, '_', pat, '_last_obs_forward')] <- data_temp[paste0(var, '_', pat)]
+      
+      data_temp <- subset(data_temp, select = -c(lower26))
+      extracted_summary <- run_lm(data_temp, var, pat, variables, 'last_obs_forward')
+      df_results <- rbind(df_results, extracted_summary)
+    }
+  }
+  
+  # Reformat the names in the dataframe to make them match for plots
   df_results$variables <- str_remove(df_results$variables, "_MCAR")
   df_results$variables <- str_remove(df_results$variables, "_MAR")
   df_results$variables <- str_remove(df_results$variables, "_MNAR")
@@ -338,13 +408,21 @@ plot_imputation_results <- function(results_data, dist_data, var, patterns, imp)
                                  levels = c("baseline", reduced_levels))
   }
 
-  colors <- list('MCAR' = c('#000000', '#DC1C13', '#F07470', '#F6BDC0'),
-              'MAR' = c('#000000', '#0000FF', '#7879FF', '#BFBFFF'),
-              'MNAR' = c('#000000', '#2EB62C', '#83D475', '#C5E8B7'))
+  if (length(imp) == 3){
+    colors <- list('MCAR' = c('#000000', '#DC1C13', '#F07470', '#F6BDC0'),
+                   'MAR' = c('#000000', '#0000FF', '#7879FF', '#BFBFFF'),
+                   'MNAR' = c('#000000', '#2EB62C', '#83D475', '#C5E8B7'))
+  } else if (length(imp) == 4){
+    colors <- list('MCAR' = c('#000000', '#DC1C13', '#F07470', '#F6BDC0', '#fbe8e7'),
+                   'MAR' = c('#000000', '#0000FF', '#7879FF', '#BFBFFF', '#e5e5ff'),
+                   'MNAR' = c('#000000', '#2EB62C', '#83D475', '#C5E8B7', '#eaf7e9'))
+  }
   
-  plots_distribution <- list()
+  plots_distribution <- vector(mode = "list", length = length(patterns))
   cbPalette <- c('black')
+  count = 0
   for (pat in patterns){
+    count = count + 1
     if (var == 'ais1'){
       plot_temp <- plot_AIS_distribution(dist_data, pat, colors[pat][[1]])
     } else if (var == 'lower01'){
@@ -352,7 +430,7 @@ plot_imputation_results <- function(results_data, dist_data, var, patterns, imp)
     } else if (var == 'lower52'){
       plot_temp <- plot_lems52_distribution(dist_data, pat, colors[pat][[1]])
     }
-    plots_distribution <- list(plots_distribution, pat = plot_temp)
+    plots_distribution[[count]] <- plot_temp
     cbPalette <- c(cbPalette, rep(colors[pat][[1]][3], length(imp)))
   }
   
@@ -369,17 +447,24 @@ plot_imputation_results <- function(results_data, dist_data, var, patterns, imp)
     geom_vline(xintercept = log(0.05)) +
     scale_colour_manual(values = cbPalette)
 
-  final_plots <- list()
+  final_plots <- vector(mode = "list", length = 2)
+  count = 0
   for (plot in c(coef_plot, pvalue_plot)){
+    count = count + 1
+    if (count == 1){
+      plot <- coef_plot
+    } else if (count == 2){
+      plot <- pvalue_plot
+    }
     final_plot <- ggarrange(plot,
                             ggarrange(plots_distribution[1][[1]],
                                       plots_distribution[2][[1]],
                                       plots_distribution[3][[1]],
-                                      nrow = length(imp), labels = toupper(letters[2:(length(imp)+1)])
+                                      nrow = length(patterns), labels = toupper(letters[2:(length(patterns)+1)])
                             ),
                             ncol = 2,
                             labels = "A")
-    final_plots <- append(final_plots, final_plot)
+    final_plots[[count]] <- final_plot
   }
   
   #final_plots <- list(coef_plot, pvalue_plot)
@@ -401,10 +486,13 @@ plot_lems01_distribution <- function(dist_data, missing, col){
   
   plot <- ggplot(long_ais, aes(x=value, fill=LEMS)) + 
     geom_histogram(position="dodge") +
-    facet_grid(rows = vars(ais1), cols = vars(LEMS), scales = 'free')
+    facet_grid(rows = vars(ais1), cols = vars(LEMS), scales = 'free')+
+    scale_fill_manual(values=col)
   
   return (plot)
 }
+
+# ------------------------------------------------------------------------------
 
 plot_AIS_distribution <- function(dist_data, missing, col){
   vec <- c("ais1", paste0("ais1_", missing), paste0("ais1_", missing, "_mean/majority"), paste0("ais1_", missing, "_regression"))
@@ -423,29 +511,64 @@ plot_AIS_distribution <- function(dist_data, missing, col){
   return (plot)
 }
 
+# ------------------------------------------------------------------------------
 
-plot_lems52_distribution <- function(missing, col){
-  vec <- c("lower52", paste0("lower52_", missing), paste0("lower52_", missing, "_mean/majority"), paste0("lower01_", missing, "_regression"))
-  subset_ais <- subset_col(vec, sygen_analysis_subset)
-  if (missing == 'MCAR'){
-    subset_ais[paste0("lower52_", missing, "_regression")] <- sygen_analysis_subset_lower52_MCAR_imputed$lower52_MCAR
-  } else if (missing == 'MAR'){
-    subset_ais[paste0("lower52_", missing, "_regression")] <- sygen_analysis_subset_lower52_MAR_imputed$lower52_MAR
-  } else if (missing == 'MNAR'){
-    subset_ais[paste0("lower52_", missing, "_regression")] <- sygen_analysis_subset_lower52_MNAR_imputed$lower52_MNAR
-  } else {
-    print("Error in type of missingness. Valid choices are MCAR, MAR and MNAR.")
-  }
+# plot_lems52_distribution <- function(dist_data, missing, col){
+#   vec <- c("lower52", paste0("lower52_", missing), paste0("lower52_", missing, "_mean/majority"), paste0("lower01_", missing, "_regression"))
+#   subset_ais <- subset_col(c('ais1', vec), dist_data)
+#   if (missing == 'MCAR'){
+#     subset_ais[paste0("lower52_", missing, "_regression")] <- sygen_analysis_subset_lower52_MCAR_imputed$lower52_MCAR
+#   } else if (missing == 'MAR'){
+#     subset_ais[paste0("lower52_", missing, "_regression")] <- sygen_analysis_subset_lower52_MAR_imputed$lower52_MAR
+#   } else if (missing == 'MNAR'){
+#     subset_ais[paste0("lower52_", missing, "_regression")] <- sygen_analysis_subset_lower52_MNAR_imputed$lower52_MNAR
+#   } else {
+#     print("Error in type of missingness. Valid choices are MCAR, MAR and MNAR.")
+#   }
+#   
+#   subset_ais$ID <- seq.int(nrow(subset_ais)[1])
+#   
+#   long_ais <- melt(subset_ais, 
+#                    id.vars = "ID",
+#                    variable.name = "LEMS")
+# 
+#   plot <- ggplot(long_ais, aes(x=value, y=LEMS)) +
+#     geom_violin() +
+#     stat_summary(fun.data=data_summary)
+#   
+#   return (plot)
+# }
+
+
+plot_lems52_distribution <- function(dist_data, missing, col){
+  vec <- c("lower52", paste0("lower52_", missing), 
+           paste0("lower52_", missing, "_mean/majority"), 
+           paste0("lower52_", missing, "_regression"), 
+           paste0("lower52_", missing, "_last_obs_forward"))
+  subset_ais <- subset_col(c('ais1', vec), dist_data)
   
   subset_ais$ID <- seq.int(nrow(subset_ais)[1])
   
   long_ais <- melt(subset_ais, 
-                   id.vars = "ID",
+                   id.vars = c("ID", 'ais1'),
                    variable.name = "LEMS")
-
-  plot <- ggplot(long_ais, aes(x=value, y=LEMS)) +
-    geom_violin() +
-    stat_summary(fun.data=data_summary)
+  
+  plot <- ggplot(long_ais, aes(x=value, fill=LEMS)) + 
+    geom_histogram(position="dodge", colour='black') +
+    facet_grid(rows = vars(ais1), cols = vars(LEMS), scales = 'free')+
+    scale_fill_manual(values=col)
   
   return (plot)
 }
+
+# ------------------------------------------------------------------------------
+
+plot_miss_upset <- function(data, cols){
+  
+  df_temp <- subset_col(cols, data)
+  
+  plot <- gg_miss_upset(df_temp, nsets = n_var_miss(df_temp))
+  
+  return (plot)
+}
+
--- main.R
+++ main.R
@@ -1,8 +1,8 @@
 ################################################################################
 # SCI - Handling missing data project 
-# L. Bourguignon
+# L. Bourguignon & L.P. Lukas 
 # First version : 06.07.2021
-# Last update : 12.10.2021
+# Last update : 31.10.2021
 # ------------------------------------------------------------------------------
 # MAIN FILE
 ################################################################################
@@ -19,6 +19,12 @@ library(nnet)
 library(ggpubr)
 library(rlang)
 library(stringr)
+library(dplyr)
+library(reshape2)
+library(naniar)
+library(networkD3)
+library(rpart)
+library(rpart.plot)
 
 set.seed(1)
 
@@ -34,6 +40,7 @@ sygen_raw <- read.csv(paste(data_path, 'Sygen/JohnKramersProject_DATA_2019-10-07
 ################################################################################
 
 source("/Users/blucie/PhD/1_SCI/6_Missing_data/code/functions.R")
+source("/Users/blucie/PhD/1_SCI/6_Missing_data/code/visual_exploration.R")
 
 ################################################################################
 # Prepare Sygen data - step 1 (visual inspection of the raw data)
@@ -64,75 +71,22 @@ sygen_analysis[cols.fac] <- lapply(sygen_analysis[cols.fac], factor)
 
 ## Visualise original missingness patterns
 
-# ref for interpretation of the folloming plot: 
-# https://cran.r-project.org/web/packages/finalfit/vignettes/missing.html
-
-sygen_analysis %>% 
-  missing_pairs("lower52", c("age", "sexcd", "level", "lower01", "ais1"))
-
-sygen_analysis %>% 
-  missing_pairs("lower52", c("age", "sexcd", "level", "lower01", "ais1"), 
-                position = "fill", )
-
-# x-axis represent patients, light blue = NA
-sygen_analysis %>%
-  missing_plot()
+missing_dist
+missing_dist_fill
+missing_heatmap
 
 # ------------------------------------------------------------------------------
 
 ## Visualise the distributions of the different variables
 
-# AIS grade, i.e. severity
-ais_sygen_raw <- ggplot(sygen_analysis, aes(ais1)) + geom_bar()
-ais_sygen_raw
-
-# Age distribution
-age_sygen_raw <- ggplot(sygen_analysis, aes(x=age)) + 
-  geom_histogram(aes(y=..density..), colour="black", fill="white")+
-  geom_density(alpha=.2, fill="#FF6666") 
-age_sygen_raw
-
-# Sex 
-sex_sygen_raw <- ggplot(sygen_analysis, aes(factor(sexcd))) + geom_bar() + 
-  scale_x_discrete(labels=c("1" = "Female", "2" = "Male"), name = 'Sex')
-sex_sygen_raw
-
-# Distribution of LEMS at week 01
-lems01_sygen_raw <- ggplot(sygen_analysis, aes(x=lower01)) + geom_density(alpha=.2, fill="#FF6666") + xlim(0,50)
-lems01_sygen_raw
-
-# Distribution of LEMS at week 52
-lems52_sygen_raw <- ggplot(sygen_analysis, aes(x=lower52)) + geom_density(alpha=.2, fill="#FF6666") + xlim(0,50)
-lems52_sygen_raw
-
-# Level of injury
-nli_sygen_raw <- ggplot(sygen_analysis, aes(splvl)) + geom_bar() + 
-  scale_x_discrete(name = 'Level of injury')
-nli_sygen_raw
-
-# Variables stratified by AIS grade
-#ggplot(sygen_analysis, aes(x=lower01, fill=ais1)) + geom_density(alpha=.3)+ xlim(0,50)
-#ggplot(sygen_analysis, aes(x=lower52, fill=ais1)) + geom_density(alpha=.3)+ xlim(0,50)
-#ggplot(sygen_analysis, aes(x=age, fill=ais1)) + geom_density(alpha=.3)
-#ggplot(sygen_analysis, aes(x=factor(sexcd), fill=ais1)) + geom_density(alpha=.3)
-#ggplot(sygen_analysis, aes(x=splvl, fill=ais1)) + geom_histogram(stat="count")
-
-# Relationship between LEMS at week 01 and week 52, coloured by AIS grade
-jitterLEMS_byAIS_sygen_raw <- ggplot(sygen_analysis, aes(lower01, lower52, colour=ais1)) + 
-  geom_jitter(width = 2, height = 2) + 
-  xlim(-2,50) +
-  ylim(-2,50)
-jitterLEMS_byAIS_sygen_raw
-
-# Change in LEMS distribution over time, by AIS grade
-lems01_byAIS_sygen_raw <- ggplot(sygen_analysis, aes(x=lower01)) + geom_density(alpha=.2, fill="#FF6666") + 
-  xlim(0,50) +
-  facet_grid(rows = vars(ais1), scales = 'free')
-lems52_byAIS_sygen_raw <- ggplot(sygen_analysis, aes(x=lower52)) + geom_density(alpha=.2, fill="#FF6666") + 
-  xlim(0,50) +
-  facet_grid(rows = vars(ais1), scales = 'free')
-distLEMS_byAIS_sygen_raw <- ggarrange(lems01_byAIS_sygen_raw, lems52_byAIS_sygen_raw, ncol = 2, nrow = 1)
-distLEMS_byAIS_sygen_raw
+ais_sygen_raw # AIS grade, i.e. severity
+age_sygen_raw # Age distribution
+sex_sygen_raw # Sex 
+lems01_sygen_raw # Distribution of LEMS at week 01
+lems52_sygen_raw # Distribution of LEMS at week 52
+nli_sygen_raw # Level of injury
+jitterLEMS_byAIS_sygen_raw # Relationship between LEMS at week 01 and week 52, coloured by AIS grade
+distLEMS_byAIS_sygen_raw # Change in LEMS distribution over time, by AIS grade
 
 ################################################################################
 # Prepare Sygen data - step 2 (selecting only complete cases for analysis)
@@ -199,29 +153,30 @@ result_imputation_lems01 <- result_imputations(data = sygen_analysis_subset,
 result_imputation_lems52 <- result_imputations(data = sygen_analysis_subset,
                                     var = 'lower52',
                                     patterns = c('MCAR', 'MAR', 'MNAR'),
-                                    imp = c('case_deletion', 'mean', 'regression'))
+                                    imp = c('case_deletion', 'mean', 'regression', 'last_obs_forward'),
+                                    raw = sygen_raw) #additional parameter is necessary in function for the last_obs_forward imputation strategy
 
 plots_ais <- plot_imputation_results(results_data = result_imputation_ais[1][[1]],
                                      dist_data = result_imputation_ais[2][[1]], 
                                      var = 'ais1',
                                      patterns = c('MCAR', 'MAR', 'MNAR'),
                                      imp = c('case_deletion', 'majority', 'regression'))
-plots_ais[1]
-plots_ais[2]
+plots_ais[[1]]
+plots_ais[[2]]
 
 plots_lems01 <- plot_imputation_results(results_data = result_imputation_lems01[1][[1]],
                                         dist_data = result_imputation_lems01[2][[1]], 
                                         var = 'lower01',
                                         patterns = c('MCAR', 'MAR', 'MNAR'),
                                         imp = c('case_deletion', 'mean', 'regression'))
-plots_lems01[1]
-plots_lems01[2]
+plots_lems01[[1]]
+plots_lems01[[2]]
 
 
 plots_lems52 <- plot_imputation_results(results_data = result_imputation_lems52[1][[1]],
                                         dist_data = result_imputation_lems52[2][[1]], 
                                         var = 'lower52',
                                         patterns = c('MCAR', 'MAR', 'MNAR'),
-                                        imp = c('case_deletion', 'mean', 'regression'))
-plots_lems52[1]
-plots_lems52[2]
+                                        imp = c('case_deletion', 'mean', 'regression', 'last_obs_forward'))
+plots_lems52[[1]]
+plots_lems52[[2]]
